b617222b8cf8d12059d9f172f765994b7c819039
Matthew Semmens <git@matavt.net>
2022-10-04 14:09:12 +0930

    Implemented send activity file via email

--- app/src/main/java/com/matavt/healthbaby12/RecordActivity.java
+++ app/src/main/java/com/matavt/healthbaby12/RecordActivity.java
@@ -1,64 +1,85 @@
 package com.matavt.healthbaby12;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RecordActivity#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class RecordActivity extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView data;
+    String dataString;
+    Button sendButton;
 
     public RecordActivity() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RecordActivity.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RecordActivity newInstance(String param1, String param2) {
-        RecordActivity fragment = new RecordActivity();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_record_activity, container, false);
+        View view = inflater.inflate(R.layout.fragment_record_activity, container, false);
+        data = view.findViewById(R.id.recordText);
+        MainMenu.hbDB.daoActivity().getAll()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        list -> {
+                            StringBuilder sb = new StringBuilder();
+                            for (EntityActivity entity: list) {
+                                sb.append(entity.toString()).append("\n");
+                            }
+                            dataString = sb.toString();
+                            data.setText(dataString);
+                        });
+
+        sendButton = view.findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(view1 -> {
+            String fileName = User.getInstance().getChildName() + "_activities.txt";
+            File file = new File(getContext().getCacheDir(), fileName);
+            try {
+                FileWriter inFile= new FileWriter(file);
+                inFile.append(dataString);
+                inFile.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String subject = User.getInstance().getChildName() + " Activity file";
+            String message = "Please see attached file containing a record of activities for " + User.getInstance().getChildName();
+            message = message + "\n regards \n" + User.getInstance().getUserName();
+
+            final Intent emailIntent = new Intent(Intent.ACTION_SEND);
+            emailIntent.setType("plain/text");
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+            emailIntent.putExtra(Intent.EXTRA_TEXT, message);
+            emailIntent.putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(getActivity(),"com.matavt.healthbaby12.fileProvider",file));
+
+            startActivity(emailIntent);
+
+        });
+
+        return view;
     }
 }
