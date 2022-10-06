3f2b38188dc03fcf5658f0e80d1cefec7fc0eda6
Matthew Semmens <git@matavt.net>
2022-10-06 20:59:01 +0930

    Graph for weight height initial implementation

--- app/src/main/java/com/matavt/healthbaby12/RecordWeightHeight.java
+++ app/src/main/java/com/matavt/healthbaby12/RecordWeightHeight.java
@@ -4,61 +4,80 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RecordWeightHeight#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class RecordWeightHeight extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    LineChart weightChart, heightChart;
+    List<Entry> weightDataList, heightDataList;
 
     public RecordWeightHeight() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RecordWeightHeight.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RecordWeightHeight newInstance(String param1, String param2) {
-        RecordWeightHeight fragment = new RecordWeightHeight();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_record_weight_height, container, false);
+        View view = inflater.inflate(R.layout.fragment_record_weight_height, container, false);
+        weightChart = view.findViewById(R.id.weightChart);
+        heightChart = view.findViewById(R.id.heightChart);
+        weightDataList = new ArrayList<Entry>();
+        heightDataList = new ArrayList<Entry>();
+
+        MainMenu.hbDB.daoHeightWeight().getAll()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        list -> {
+                            for (EntityHeightWeight entity:list) {
+                                int age = (int) Duration.between(entity.date.toInstant(),User.getInstance().getChildDoB().toInstant()).toDays();
+                                weightDataList.add(new Entry(age,entity.weight));
+                                heightDataList.add(new Entry(age,entity.height));
+                                Log.i("age",String.valueOf(age));
+                                Log.i("entity",entity.toString());
+                            }
+                            LineDataSet weightDataSet = new LineDataSet(weightDataList, "Weight");
+                            LineDataSet heightDataSet = new LineDataSet(heightDataList, "Height");
+                            LineData weightData = new LineData(weightDataSet);
+                            LineData heightData = new LineData(heightDataSet);
+                            weightChart.setData(weightData);
+                            heightChart.setData(heightData);
+                            weightChart.invalidate();
+                            heightChart.invalidate();
+                        }
+
+                        );
+
+        LineDataSet weightDataSet = new LineDataSet(weightDataList, "Weight");
+        LineDataSet heightDataSet = new LineDataSet(heightDataList, "Height");
+        LineData weightData = new LineData(weightDataSet);
+        LineData heightData = new LineData(heightDataSet);
+        weightChart.setData(weightData);
+        heightChart.setData(heightData);
+        weightChart.invalidate();
+        heightChart.invalidate();
+
+        return view;
     }
 }
